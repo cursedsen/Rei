e897fda9946c7e076aace3175473ff7f102db99d
Senko <57877797+OhSenko@users.noreply.github.com>
2024-12-12 16:21:45 +0100

    echo command

---
+++ commands/fun/echo.js
@@ -0,0 +1,37 @@
+import { sendMessage } from '../../functions/reiMessageMaker.js';
+
+export default {
+    name: 'echo',
+    description: 'Repeats what you say',
+    usage: '<message>',
+    execute: async (message, args) => {
+        if (!message.member.permissions.has('Administrator')) {
+            return await sendMessage(message, {
+                title: 'Error',
+                description: 'You do not have permission to use this command.',
+                color: 0xFF0000
+            });
+        }
+        if (!args.length) {
+            return await sendMessage(message, {
+                title: 'Error',
+                description: 'Please provide a message to echo.',
+                color: 0xFF0000
+            });
+        }
+
+        const echoMessage = args.join(' ');
+        
+        try {
+            await message.delete();
+            await message.channel.send(echoMessage);
+        } catch (error) {
+            console.error(error);
+            await sendMessage(message, {
+                title: 'Error',
+                description: 'Failed to send message.',
+                color: 0xFF0000
+            });
+        }
+    }
+};
