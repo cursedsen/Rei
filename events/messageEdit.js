933689b4656d176a40a586968a8ab749eee9f32c
Senko <57877797+OhSenko@users.noreply.github.com>
2024-12-13 08:43:18 +0100

    removed one "\n"

--- events/messageEdit.js
+++ events/messageEdit.js
@@ -24,7 +24,7 @@ export default {
                 title: '✏️ Message Edited',
                 description: `**Original message posted on**\n${oldMessage.createdAt.toUTCString()}\n` +
                     `(<t:${Math.floor(oldMessage.createdTimestamp / 1000)}:R>)\n\n` +
-                    `**Author**\n<@${oldMessage.author.id}> | ${oldMessage.author.tag}\n\`\`\`${oldMessage.author.id}\`\`\`\n\n` +
+                    `**Author**\n<@${oldMessage.author.id}> | ${oldMessage.author.tag}\n\`\`\`${oldMessage.author.id}\`\`\`\n` +
                     `**Message Location**\n${oldMessage.channel}${oldMessage.reference ? ` | [In reply to](https://discord.com/channels/${oldMessage.guild.id}/${oldMessage.channel.id}/${oldMessage.reference.messageId})` : ' | '}` +
                     `[Jump to Message](${oldMessage.url})\n\n` +
                     `**Before**\n${oldMessage.content || 'No text content'}\n\n` +
