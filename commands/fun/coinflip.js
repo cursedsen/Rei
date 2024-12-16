20d322cdc23df064309d2958946d4a180a468246
Senko <57877797+OhSenko@users.noreply.github.com>
2024-12-16 14:13:26 +0100

    add coinflip back :D

---
+++ commands/fun/coinflip.js
@@ -0,0 +1,15 @@
+import { sendMessage } from '../../functions/reiMessageMaker.js';
+
+export default {
+    name: 'coinflip',
+    description: 'Gambling addiction :3',
+    execute: async (message) => {
+        const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
+
+        await sendMessage(message, {
+            title: `🪙 Flip result:`,
+            description: `You got **${result}**!`,
+            color: 0x2B2D31
+        });
+    }
+};
