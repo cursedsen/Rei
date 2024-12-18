78a48242aa528829c42a073bddb16dbcb51b25e6
Senko <57877797+OhSenko@users.noreply.github.com>
2024-12-18 14:36:09 +0100

    bean command

---
+++ commands/fun/bean.js
@@ -0,0 +1,53 @@
+import { sendMessage } from "../../functions/reiMessageMaker.js";
+import { readFileSync } from 'fs';
+
+export default {
+    name: 'bean',
+    description: 'Bean a user.',
+    category: 'fun',
+    usage: '<user> [reason]',
+    execute: async (message, args) => {
+        if (!message.member.permissions.has('BanMembers')) {
+            return await sendMessage(message, {
+                title: 'Access Denied',
+                description: 'You do not have permission to use this command.',
+                color: 0xFF0000,
+            });
+        }
+        
+        if (!args[0]) {
+            return await sendMessage(message, {
+                title: 'Error',
+                description: 'Please provide a valid user tag or ID to ban.',
+                color: 0xFF0000,
+            });
+        }
+
+        let target = message.mentions.members.first();
+
+        if (!target) {
+            target = await message.guild.members.fetch(args[0]).catch(() => null);
+        }
+
+        if (!target) {
+            return await sendMessage(message, {
+                title: 'Error',
+                description: 'Could not find that user.',
+                color: 0xFF0000,
+            });
+        }
+
+        if (target.id === message.author.id) {
+            return await sendMessage(message, {
+                content: 'Nice try.',
+            });
+        }
+
+        const strings = JSON.parse(readFileSync('./things/strings.json', 'utf8'));
+        const funnyRandomAction = strings.user_was_x[Math.floor(Math.random() * strings.user_was_x.length)];
+
+        await sendMessage(message, {
+            content: `${target.user.tag} was ${funnyRandomAction}`,
+        });
+    }
+};
