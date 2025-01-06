f3486fd30b35f8148e5c39d498aa3e94dd3c42e8
Senko <57877797+cursedsen@users.noreply.github.com>
2025-01-06 15:01:37 +0100

    add alias to coinflip

--- commands/fun/coinflip.js
+++ commands/fun/coinflip.js
@@ -4,6 +4,7 @@ export default {
     name: 'coinflip',
     description: 'Gambling addiction :3',
     category: 'fun',
+    aliases: ['cf', 'flip'],
     execute: async (message) => {
         const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
 
