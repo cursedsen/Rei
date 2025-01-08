20b1b83d9470ee5ff9d9f3a3093f804a3364ab88
Senko <57877797+cursedsen@users.noreply.github.com>
2025-01-08 12:51:47 +0100

    buh

---
+++ commands/fun/yesno.js
@@ -0,0 +1,15 @@
+import { sendMessage } from '../../functions/reiMessageMaker.js';
+
+export default {
+    name: 'yesno',
+    description: 'Yes or no?',
+    category: 'fun',
+    execute: async (message) => {
+        const result = Math.random() < 0.5 ? 'Yes' : 'No';
+
+        await sendMessage(message, {
+            content: result
+        });
+    }
+};
+
