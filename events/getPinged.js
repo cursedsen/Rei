2385e97faa5f7ba3f040d71735486016404b07b8
Sen <57877797+cursedsen@users.noreply.github.com>
2025-01-11 14:57:23 +0100

    make it ignore @everyone and replies

--- events/getPinged.js
+++ events/getPinged.js
@@ -4,8 +4,10 @@ import { sendMessage } from "../functions/reiMessageMaker.js";
 export default {
     name: "messageCreate",
     async execute(message) {
-        if (!message.mentions.has(message.client.user) || message.author.bot)
-            return;
+        if (message.author.bot) return;
+        if (message.content.includes('@everyone') || message.content.includes('@here')) return;
+        if (message.reference || message.type === 'REPLY') return;
+        if (!message.mentions.has(message.client.user.id)) return;
 
         const strings = JSON.parse(
             readFileSync("./things/strings.json", "utf8")
