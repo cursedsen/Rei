d14a8fbd5b2992090946592749c78055ac94ffa5
Sen <57877797+cursedsen@users.noreply.github.com>
2025-01-11 15:54:55 +0100

    added a blacklist lol

--- functions/permissionHandler.js
+++ functions/permissionHandler.js
@@ -1,7 +1,18 @@
 import { isBotMaster } from '../config/botMasters.js';
 import { sendMessage } from './reiMessageMaker.js';
+import { readFileSync } from 'fs';
 
 export async function checkPermissions(message, command) {
+    const blacklist = JSON.parse(readFileSync('./things/blacklist.json', 'utf8'));
+    if (blacklist.blacklisted.includes(message.author.id)) {
+        await sendMessage(message, {
+            title: 'Access Denied',
+            description: 'You are not permitted to use commands.',
+            color: 0xFF0000,
+        });
+        return false;
+    }
+
     if (isBotMaster(message.author.id)) {
         return true;
     }
@@ -24,4 +35,4 @@ export async function checkPermissions(message, command) {
     }
 
     return true;
-} 
+}
