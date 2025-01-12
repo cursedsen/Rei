f280592f85ae328e905413431517d79ed0caf65a
JackDotJS <jackdotjs@proton.me>
2025-01-12 10:27:33 -0800

    major refactor
    Signed-off-by: JackDotJS <jackdotjs@proton.me>

--- config/botMasters.js
+++
@@ -1,7 +0,0 @@
-export const botMasters = [
-    'youruserid', // change to your own, or add more if you have multiple bot masters
-];
-
-export function isBotMaster(userId) {
-    return botMasters.includes(userId);
-}
